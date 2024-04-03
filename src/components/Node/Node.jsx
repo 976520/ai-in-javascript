85ede6c21b3584016a39318941ee7fa06d0fd50a
송재욱 <123460320+976520@users.noreply.github.com>
2024-04-04 02:02:40 +0900

    rename

--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -1,20 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-
-const NodeContainer = styled.div`
-  position: relative;
-  border: 2px solid #333;
-  border-radius: 50%;
-  width: 100px;
-  height: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 10px;
-  text-align: center;
-  background-color: #f0f0f0;
-`;
+import { NodeContainer } from "./NodeStyle";
 
 const Node = ({ inputs, weights, onOutput }) => {
   const [output, setOutput] = useState(0);
---
+++ src/components/Node/NodeStyle.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const NodeContainer = styled.div`
+  position: relative;
+  border: 2px solid #333;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  text-align: center;
+  background-color: #f0f0f0;
+`;
