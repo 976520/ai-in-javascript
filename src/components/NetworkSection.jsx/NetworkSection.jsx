9939fcc23d6a574fb15ede0c5f223f2adf40c435
송재욱 <123460320+976520@users.noreply.github.com>
2024-10-05 00:02:12 +0900

    layer

--- src/components/NetworkSection.jsx/NetworkSection.jsx
+++ src/components/NetworkSection.jsx/NetworkSection.jsx
@@ -4,22 +4,38 @@ import { Network, Layer } from "./NetworkSectionStyle";
 export const NetworkSection = ({
   inputs,
   layer1Outputs,
+  layer2Outputs,
+  layer3Outputs,
   weights1,
   handleLayer1Output,
   weights2,
   handleLayer2Output,
+  weights3,
+  handleLayer3Output,
+  weights4,
+  handleLayer4Output,
 }) => (
   <Network>
     <Layer>
       <Node inputs={inputs} weights={weights1[0]} onOutput={(output) => handleLayer1Output(0, output)} />
       <Node inputs={inputs} weights={weights1[1]} onOutput={(output) => handleLayer1Output(1, output)} />
       <Node inputs={inputs} weights={weights1[2]} onOutput={(output) => handleLayer1Output(2, output)} />
-      <Node inputs={inputs} weights={weights1[0]} onOutput={(output) => handleLayer1Output(0, output)} />
-      <Node inputs={inputs} weights={weights1[1]} onOutput={(output) => handleLayer1Output(1, output)} />
-      <Node inputs={inputs} weights={weights1[2]} onOutput={(output) => handleLayer1Output(2, output)} />
+      <Node inputs={inputs} weights={weights1[3]} onOutput={(output) => handleLayer1Output(3, output)} />
     </Layer>
     <Layer>
-      <Node inputs={layer1Outputs} weights={weights2[0]} onOutput={handleLayer2Output} />
+      <Node inputs={layer1Outputs} weights={weights2[0]} onOutput={(output) => handleLayer2Output(0, output)} />
+      <Node inputs={layer1Outputs} weights={weights2[1]} onOutput={(output) => handleLayer2Output(1, output)} />
+      <Node inputs={layer1Outputs} weights={weights2[2]} onOutput={(output) => handleLayer2Output(2, output)} />
+      <Node inputs={layer1Outputs} weights={weights2[3]} onOutput={(output) => handleLayer2Output(3, output)} />
+    </Layer>
+    <Layer>
+      <Node inputs={layer2Outputs} weights={weights3[0]} onOutput={(output) => handleLayer3Output(0, output)} />
+      <Node inputs={layer2Outputs} weights={weights3[1]} onOutput={(output) => handleLayer3Output(1, output)} />
+      <Node inputs={layer2Outputs} weights={weights3[2]} onOutput={(output) => handleLayer3Output(2, output)} />
+      <Node inputs={layer2Outputs} weights={weights3[3]} onOutput={(output) => handleLayer3Output(3, output)} />
+    </Layer>
+    <Layer>
+      <Node inputs={layer3Outputs} weights={weights4[0]} onOutput={handleLayer4Output} />
     </Layer>
   </Network>
 );
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,8 +5,9 @@ import React, { useState } from "react";
 
 const Main = () => {
   const [layer1Outputs, setLayer1Outputs] = useState([0, 0, 0, 0]);
-
-  const [layer2Outputs, setLayer2Outputs] = useState([0]);
+  const [layer2Outputs, setLayer2Outputs] = useState([0, 0, 0, 0]);
+  const [layer3Outputs, setLayer3Outputs] = useState([0, 0, 0, 0]);
+  const [layer4Outputs, setLayer4Outputs] = useState([0]);
 
   const [weights1, setWeights1] = useState([
     [0.5, 0.8, 0.3],
@@ -15,7 +16,21 @@ const Main = () => {
     [0.2, 0.3, 0.4],
   ]);
 
-  const [weights2, setWeights2] = useState([[0.6, 0.9, 0.1, 0.2]]);
+  const [weights2, setWeights2] = useState([
+    [0.6, 0.9, 0.1, 0.2],
+    [0.5, 0.4, 0.3, 0.2],
+    [0.1, 0.2, 0.3, 0.4],
+    [0.3, 0.5, 0.7, 0.9],
+  ]);
+
+  const [weights3, setWeights3] = useState([
+    [0.2, 0.4, 0.6, 0.8],
+    [0.3, 0.5, 0.7, 0.9],
+    [0.1, 0.2, 0.3, 0.4],
+    [0.4, 0.6, 0.8, 0.1],
+  ]);
+
+  const [weights4, setWeights4] = useState([[0.1, 0.2, 0.3, 0.4]]);
 
   const [inputs, setInputs] = useState([0, 0, 0]);
 
@@ -27,15 +42,28 @@ const Main = () => {
     setLayer1Outputs(newOutputs);
   };
 
-  const handleLayer2Output = (output) => {
-    setLayer2Outputs([output]);
+  const handleLayer2Output = (index, output) => {
+    const newOutputs = [...layer2Outputs];
+    newOutputs[index] = output;
+    setLayer2Outputs(newOutputs);
+  };
+
+  const handleLayer3Output = (index, output) => {
+    const newOutputs = [...layer3Outputs];
+    newOutputs[index] = output;
+    setLayer3Outputs(newOutputs);
+  };
+
+  const handleLayer4Output = (output) => {
+    setLayer4Outputs([output]);
   };
 
   const trainNetwork = () => {
     const targetOutput = 1;
     const layer1Errors = layer1Outputs.map((output) => targetOutput - output);
-
-    const layer2Error = targetOutput - layer2Outputs[0];
+    const layer2Errors = layer2Outputs.map((output) => targetOutput - output);
+    const layer3Errors = layer3Outputs.map((output) => targetOutput - output);
+    const layer4Error = targetOutput - layer4Outputs[0];
 
     const newWeights1 = weights1.map((weights, index) => {
       return weights.map((weight, weightIndex) => {
@@ -50,14 +78,34 @@ const Main = () => {
       });
     });
 
-    const newWeights2 = weights2.map((weights) => {
+    const newWeights2 = weights2.map((weights, index) => {
+      return weights.map((weight, weightIndex) => {
+        return (
+          weight +
+          learningRate * layer2Errors[index] * layer2Outputs[index] * (1 - layer2Outputs[index]) * layer1Outputs[index]
+        );
+      });
+    });
+
+    const newWeights3 = weights3.map((weights, index) => {
+      return weights.map((weight, weightIndex) => {
+        return (
+          weight +
+          learningRate * layer3Errors[index] * layer3Outputs[index] * (1 - layer3Outputs[index]) * layer2Outputs[index]
+        );
+      });
+    });
+
+    const newWeights4 = weights4.map((weights) => {
       return weights.map((weight, index) => {
-        return weight + learningRate * layer2Error * layer2Outputs[0] * (1 - layer2Outputs[0]) * layer1Outputs[index];
+        return weight + learningRate * layer4Error * layer3Outputs[index] * (1 - layer3Outputs[index]);
       });
     });
 
     setWeights1(newWeights1);
     setWeights2(newWeights2);
+    setWeights3(newWeights3);
+    setWeights4(newWeights4);
   };
 
   const handleInputChange = (index, value) => {
@@ -73,12 +121,18 @@ const Main = () => {
       <NetworkSection
         inputs={inputs}
         layer1Outputs={layer1Outputs}
+        layer2Outputs={layer2Outputs}
+        layer3Outputs={layer3Outputs}
         weights1={weights1}
         handleLayer1Output={handleLayer1Output}
         weights2={weights2}
         handleLayer2Output={handleLayer2Output}
-      />{" "}
-      <h2>{layer2Outputs[0].toFixed(4)}</h2>
+        weights3={weights3}
+        handleLayer3Output={handleLayer3Output}
+        weights4={weights4}
+        handleLayer4Output={handleLayer4Output}
+      />
+      <h2>{layer4Outputs[0].toFixed(4)}</h2>
     </div>
   );
 };
