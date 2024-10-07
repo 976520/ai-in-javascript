420b2c624baac61f08241562facc8913794e064d
송재욱 <123460320+976520@users.noreply.github.com>
2024-10-07 13:34:40 +0900

    margin

--- src/components/Node/NodeStyle.jsx
+++ src/components/Node/NodeStyle.jsx
@@ -10,7 +10,7 @@ export const NodeContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin: 10px;
+  margin: 20px 50px 20px 20px;
   text-align: center;
   background-color: #f0f0f0;
 `;
